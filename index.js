bfec68255bd734f0ee20c84ebbc9e08c6d7c750a
twon1011 <twon1011@aol.com>
2018-05-14 16:53:21 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+let button = document.createElement("button");
+let btnText = document.createTextNode("Add Square");
+button.appendChild(btnText);
+document.body.appendChild(button);
+let buttondiv = document.createElement("div");
+btn.addEventListener("buttondiv")
+
+})
